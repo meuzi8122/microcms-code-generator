48199d801ee3f1c518720d3d75deac7688d6b518
meuzi8122 <kagiakete1234@gmail.com>
2023-05-16 18:48:22 +0900

    ノード作成とコード文字列作成を分離

--- src/generator.ts
+++ src/generator.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import { ListFormat, ScriptTarget, TypeAliasDeclaration, createPrinter, createSourceFile, factory } from "typescript";
+import { ListFormat, NodeArray, ScriptTarget, TypeAliasDeclaration, createPrinter, createSourceFile, factory } from "typescript";
 import { CustomAliasFactory, ImageAlias } from "./factory/alias";
 import { AnyPropertyFactory, BoolPropertyFactory, NumberPropertyFactory, ReferencePropertyFactory, StringPropertyFactory } from "./factory/property";
 import type { Schema } from "./type";
@@ -43,10 +43,14 @@ export class CMSCodeGenerator {
         return CustomAliasFactory.generate(schema.name, properties);
     }
 
+    generateNodes(): NodeArray<TypeAliasDeclaration> {
+        return factory.createNodeArray([ImageAlias, ...this.schemas.map(schema => this.generateAlias(schema))]);
+    }
+
     generateCode(): string {
         return printer.printList(
             ListFormat.MultiLine,
-            factory.createNodeArray([ImageAlias, ...this.schemas.map(schema => this.generateAlias(schema))]),
+            this.generateNodes(),
             createSourceFile("", "", ScriptTarget.ESNext, true)
         );
     }
